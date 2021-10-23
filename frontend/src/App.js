0a67c27a4ab72a226ec6d92c73341833ad65d515
gimmeabeer <fitmerdox@outlook.com>
2021-10-23 12:33:35 +0700

    Update package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,21 @@
-import "./App.css";
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
-class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     items: [],
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   fetch("http://127.0.0.1:8080/api/v1/product/1")
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       this.setState({
-  //         items: json,
-  //       });
-  //     });
-  // }
-
-  render() {
-    // var items = this.state;
-    // console.log(items);
-
-    return <div>Hello</div>;
-  }
+export default function Rounting() {
+  return (
+    <div className="wrap">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
-
-export default App;
