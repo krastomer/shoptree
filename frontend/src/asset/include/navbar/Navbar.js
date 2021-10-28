57fe52a9d138bdc2973cc6a708a934b60205f2ba
Orthadoz <Orthadoz123@gmail.com>
2021-10-29 05:44:41 +0700

    update review, navbar

--- frontend/src/asset/include/navbar/Navbar.js
+++ frontend/src/asset/include/navbar/Navbar.js
@@ -1,11 +1,16 @@
 import "./Navbar.css";
 import React, { useState } from "react";
+import Applogo from "../../LogoBanner.png";
 
 export default function Navbar() {
   return (
-        <div className="font-body container mx-auto">
-          Navbar Test 
-        </div>
+        <nav className ="bg-white border-indigo-500 px-2 md:px-4 lg:px-5">
+          <div className="container mx-auto flex flex-wrap items-center justify-between">
+            <a href="#" className="flex w-1/4">
+            <img src={Applogo} alt="Logo"/>
+            </a>
+          </div>
+        </nav>
   )
 }
 
