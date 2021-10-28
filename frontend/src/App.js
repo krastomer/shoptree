05fb95421cf4b7da33681d124df862e876fc3306
kgvkgv20 <kgvkgv20@gmail.com>
2021-10-29 03:51:25 +0700

    add navbar file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Home from "./pages/Home/Home";
+<<<<<<< Updated upstream
 export default function App() {
+=======
+import Navbar from "./asset/include/navbar/Navbar";
+
+export default function Rounting() {
+>>>>>>> Stashed changes
   return (
     <div className="wrap">
       <BrowserRouter>
@@ -16,6 +22,9 @@ export default function App() {
           </Route>
           <Route path="/register">
             <Register />
+          </Route>
+          <Route path="/navbar">
+            <Navbar />
           </Route>
         </Switch>
       </BrowserRouter>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,46 @@
 module.exports = {
   mode : "jit",
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       body: ['IBM Plex Sans Thai', 'Kanit'], 
     },
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      textColor: ['active'],
+      outline: ['hover', 'active'],
+    },
+
   },
   variants: {
-    extend: {},
+    extend: {
+      flexWrap: ['hover', 'focus'],
+      flex: ['hover', 'focus'],
+      alignSelf: ['hover', 'focus'],
+      textDecoration: ['focus-visible'],
+      ringColor: ['hover', 'active'],
+      gridTemplateRows: ['hover', 'focus'],
+      gridTemplateColumns: ['hover', 'focus'],
+      borderColor: ['active'],
+      margin: ['hover', 'focus'],
+      gap: ['hover', 'focus'],
+      fill: ['hover', 'focus'],
+      colors: {
+        'brown-theme': '#9D5B53',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
+
   ],
 }
