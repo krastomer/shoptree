930a016db1a2e3dd4893c1f2761fba9a0cab75a5
gimmeabeer <fitmerdox@outlook.com>
2021-10-29 17:12:21 +0700

    Update script

--- script/01init_mariadb.sh
+++ script/01init_mariadb.sh
@@ -5,4 +5,4 @@ dir="/app/shoptree/database"
 password="password"
 TZ="Asia/Bangkok"
 
-docker run --name $database -v $dir:/var/lib/mysql -e MARIADB_ROOT_PASSWORD=$password -e TZ=$TZ --restart=always -dp 3306:3306 mariadb:10.5.12
+docker run --name $database -v $dir:/var/lib/mysql -v ${pwd}/database:/docker-entrypoint-initdb.d -e MARIADB_ROOT_PASSWORD=$password -e TZ=$TZ --restart=always -dp 3306:3306 mariadb:10.5.12
