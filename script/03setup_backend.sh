0aefd94d37eafcba3d10f813d9b8ed708e70d564
gimmeabeer <fitmerdox@outlook.com>
2021-10-29 17:36:27 +0700

    Update script

--- script/03setup_backend.sh
+++ script/03setup_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -f backend/Dockerfile -t shoptree-backend ./backend
-docker run -p 8080:8080 --rm shoptree-backend
+docker run -p 8080:8080 --restart=always shoptree-backend
