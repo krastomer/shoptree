eb6e9bf0b0ead0c34445583794896c16a2f21131
gimmeabeer <fitmerdox@outlook.com>
2021-11-01 13:29:36 +0700

    Remove pkg

--- backend/pkg/repository/mariadb/error.go
+++
@@ -1,9 +0,0 @@
-package mariadb
-
-import "errors"
-
-var (
-	ErrQueryNotFound       = errors.New("query not found")
-	ErrInsertFailed        = errors.New("insert failed")
-	ErrInternalServerError = errors.New("internal server error")
-)
