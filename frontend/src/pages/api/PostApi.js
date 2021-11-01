c0d75be683aaf68970894b8a0bbe74d950d3bc33
nitirod1 <53013160+nitirod1@users.noreply.github.com>
2021-11-02 03:03:33 +0700

    api folder&&register

--- frontend/src/pages/api/PostApi.js
+++ frontend/src/pages/api/PostApi.js
@@ -10,6 +10,29 @@ export function getProduct(item) {
     .then((result) => console.log(result))
     .catch((error) => console.log("error", error));
 }
+export function postLogin(item) {
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Cookie",
+    "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsInVzZXIiOiJrcmFzdG9tZXJAZ21haWwuY29tIiwiYXVkIjoiQ3VzdG9tZXIiLCJleHAiOjE2MzU4MjgwODcsImlzcyI6InNob3B0cmVlIn0.rOIblmfOV0wAZtf7xKf6MpH8Od2fq4mkf_1miG45QXQ"
+  );
+
+  var formdata = new FormData();
+  formdata.append("Username", item[0]);
+  formdata.append("Password", item[1]);
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formdata,
+    redirect: "follow",
+  };
+
+  fetch("http://spaceship.trueddns.com:23720/api/v1/auth/login", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
 export function postRegister(item) {
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
@@ -28,10 +51,7 @@ export function postRegister(item) {
     redirect: "follow",
   };
 
-  fetch(
-    "http://spaceship.trueddns.com:23720/api/v1/auth/register",
-    requestOptions
-  )
+  fetch("http://spaceship.trueddns.com:23720/api/v1/customers/", requestOptions)
     .then((response) => response.text())
     .then((result) => console.log(result))
     .catch((error) => console.log("error", error));
