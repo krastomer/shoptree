fafe2e00bb86a7f4daae3a862befcaf95e3b3bbb
nitirod1 <53013160+nitirod1@users.noreply.github.com>
2021-11-02 10:23:06 +0700

    edit file name

---
+++ frontend/src/pages/home/allProduct.js
@@ -0,0 +1,39 @@
+export default [
+    {
+      id: "1",
+      name: 'ต้นไม้ตกแต่ง T001',
+      imageSrc: 'https://blog.ghbank.co.th/wp-content/uploads/2021/02/10-air-purification-trees-for-home-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$3,500.00',
+      status: '40:00',
+      unittime: 'นาที',
+    },
+    {
+      id: "2",
+      name: 'ต้นไม้ประดับ T002',
+      imageSrc: 'https://lh3.googleusercontent.com/proxy/V1xh8rMzABPkhkeHhQOa66r2VaHAOZA7i-tg91kdMwFj7vmCedwZ7Poy_opjd9n0hHaUKT7QUOof-1qKfk8KKip8dEU-SY-dgjXTPKeaJSvpyk-vRHTIEkjjGMJpRdBwgZQ',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$300.00',
+      status: '',
+      unittime: '',
+    },
+    {
+      id: "3",
+      name: 'ต้นไม้ประดับ T003',
+      imageSrc: 'https://www.jorakay.co.th/ckfinder/userfiles/images/%E0%B8%9E%E0%B8%B2%E0%B8%A2%E0%B8%B8%E0%B8%9D%E0%B8%99%E0%B9%80%E0%B8%AA%E0%B8%B5%E0%B9%88%E0%B8%A2%E0%B8%87%E0%B8%97%E0%B8%B3%E0%B8%95%E0%B9%89%E0%B8%99%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B8%A5%E0%B9%89%E0%B8%A1%E0%B8%97%E0%B8%B1%E0%B8%9A%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%20%E0%B8%A1%E0%B8%B5%E0%B8%A7%E0%B8%B4%E0%B8%98%E0%B8%B5%E0%B8%9B%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B8%AD%E0%B8%A2%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B9%84%E0%B8%A3%20%20%E0%B9%83%E0%B8%99%201.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$1,200.00',
+      status: '',
+      unittime: '',
+    },
+    {
+      id: "4",
+      name: 'ต้นไม้ประดับ T004',
+      imageSrc: 'https://s3.theasianparent.com/tap-assets-prod/wp-content/uploads/sites/25/2021/03/%E0%B8%95%E0%B9%89%E0%B8%99%E0%B9%84%E0%B8%A1%E0%B9%891-1.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$5,500.00',
+      status: '12:30',
+      unittime: 'นาที',
+    },
+    // More products...
+  ]
