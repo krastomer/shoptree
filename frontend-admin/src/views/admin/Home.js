0e5a1d6457ddce3cdcbdaf47272b8902f9906ae7
Orthadoz <orthadoz123@gmail.com>
2021-11-19 15:35:43 +0700

    setup admin page

---
+++ frontend-admin/src/views/admin/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AdminNavbar from '../../components/navbar/AdminNavbar';
+
+export default function Home() {
+  return (
+    <div className="font-body">
+        <AdminNavbar />
+            ยินดีตอนรับเข้าสู่ชมรม
+    </div>
+  );
+}
+
