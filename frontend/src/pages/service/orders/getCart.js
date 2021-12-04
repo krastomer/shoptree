9d9d8cb9abaf81252e5beb8e153e4b1ab110c01e
nitirod1 <53013160+nitirod1@users.noreply.github.com>
2021-12-04 22:39:56 +0700

    sasas

--- frontend/src/pages/service/orders/getCart.js
+++ frontend/src/pages/service/orders/getCart.js
@@ -1,20 +1,26 @@
 import axios from "axios";
 
-const accesstoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsInVzZXIiOiJrcmFzdG9tZXJAZ21haWwuY29tIiwiYXVkIjoiQ3VzdG9tZXIiLCJleHAiOjE2Mzg2MjA5MzQsImlzcyI6InNob3B0cmVlIn0.dzbBuHH0TkUzhe8goPdAkl9rXwuyLoB6HzAK4wDVMV8';
+const accesstoken = localStorage.getItem("token")
 const appURL = 'http://spaceship.trueddns.com:23720/api';
 const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': accesstoken,
-    'Cookie': accesstoken
+    'Cookie': `jwt=${accesstoken}`
 }
+export const login = (username, password) => {
+    return axios
+      .get(appURL + "/v1/orders",
+      )
+      .then((response) => {
+        console.log("kuy")
   
+        return response.data;
+      });
+  };
 export const getCart = async () => {
     let response;
     try {
         response = await axios.get(`${appURL}/v1/orders`,{
-            headers: headers
+        headers: headers
         })
-        console.log(response.data);
     } catch (error) {
         console.error(error)
     }
